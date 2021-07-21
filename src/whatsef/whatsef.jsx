7669c587a0eaab1d4a7f5c932206c67920b2d411
Gustavo Ribeiro <gustavoribeiro1011@gmail.com>
2021-07-20 22:26:02 -0300

    default

--- src/whatsef/whatsef.jsx
+++ src/whatsef/whatsef.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
 import PageHeader from "./../template/pageHeader";
 import Logo from "./../template/Logo";
 import WhatsefForm from "./whatsefForm";
 
-export default (props) => (
-  <div>
-    <Logo />
-    <PageHeader name="Form" />
-    <WhatsefForm />
-  </div>
-);
+
+export default class Whatsef extends Component {
+ 
+  constructor(props) {
+    super(props)
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(field, e){    		
+    let fields = this.state.fields;
+    fields[field] = e.target.value;        
+    this.setState({fields});
+    console.log(fields)
+  }
+
+  handleSubmit(){
+  
+    
+  }
+  
+  render() {
+    
+    return (
+      <div>
+        <Logo />
+        <PageHeader name="Formulário" />
+        <WhatsefForm 
+        handleSubmit={this.handleSubmit.bind(this)} 
+        handleChange={this.handleChange.bind("name")}      
+        handleChange={this.handleChange.bind("email")}    
+        handleChange={this.handleChange.bind("comments")}  
+        fields={this.state.handleSubmit}
+         />
+      </div>
+    )
+  }
+}
+
--- src/whatsef/whatsefForm.jsx
+++ src/whatsef/whatsefForm.jsx
@@ -1,4 +1,52 @@
 import React from "react";
-import Form from "../template/Form";
 
-export default (props) => <Form />;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { Form, Button } from "react-bootstrap";
+import Grid from "./../template/grid";
+
+export default props => (
+  
+  <Form onSubmit={props.handleSubmit}>
+    <Grid cols="12 10 8">
+      <Form.Group>
+        <Form.Label>Nome</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Seu nome"          
+          value={props.fields}
+          onChange={props.handleChange.bind(this, "name")}
+        ></Form.Control>
+        <Form.Text className="text-danger">
+        </Form.Text>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>E-mail</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Seu e-mail"
+          value={props.fields}
+          onChange={props.handleChange.bind(this, "email")}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Comentários</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          placeholder="Comentários se houver"
+          onChange={props.handleChange.bind(this, "comments")}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group>
+        <Button
+          variant="success"
+          className="mt-3"
+          type="submit"
+        >
+          <FontAwesomeIcon icon={faWhatsapp} /> Enviar via WhatsApp
+        </Button>
+      </Form.Group>
+    </Grid>
+  </Form>
+)
